10215c767313275b79dbfdf5dea0cb179bc4fe1a
Sanclayr Vieira Sindarsic <sanclayr@gmail.com>
2017-10-11 15:52:34 -0300

    Angular 2 - Diretivas

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
-import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    TesteComponent
   ],
   imports: [
     BrowserModule
